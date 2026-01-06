2bd053f6670e8c18078ff45f6aec07ee7b37db85
shiv-coder <shivi359@gmail.com>
2026-01-06 14:22:29 -0500

    Createed some variable and priniting thgeir types

---
+++ Module1/Activity2.py
@@ -0,0 +1,16 @@
+# STEP 1: Create different data types
+numbers = [10,20,30,50]     # list
+student = {"name":"shivi","age":30}  # dictionary
+grades = (80,45,60)            # tuple
+message = "Welcome to the Machine learning"  # string
+ 
+# STEP 2: Print data types
+print(type(numbers))
+print(type(student))
+print(type(grades))
+print(type(message))
+ 
+ 
+# STEP 3: Access list and dictionary elements
+         # first item in list
+   # get value from dictionary
