f1fc2b8c5bedd3ba63e0ce218708c34c9836dff4
shiv-coder <shivi359@gmail.com>
2026-01-06 14:05:43 -0500

    completed Activity1.py

---
+++ Module1/Activity1.py
@@ -0,0 +1,19 @@
+# STEP 1: Create basic variables
+# Variables store data. Here we create name, age, and GPA.
+ 
+name = "shivi"
+age = 30
+gpa = 3.8
+ 
+ 
+# STEP 2: Print each variable
+# print() is used to display values to the screen
+ 
+print(name)
+print(age)
+print(gpa)
+
+# STEP 3: Create a sentence using f-strings
+# f-strings allow combining text and variables
+
+print(f"My name is {name} and I am {age} years old and have gpa {gpa}")
